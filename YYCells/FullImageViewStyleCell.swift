f79579db70605908cd03b4c3547fe23facff4860
yalin macbook13 <yangyalin@nationsky.com>
2018-05-04 14:34:43 +0800

    init cells

---
+++ YYCells/FullImageViewStyleCell.swift
@@ -0,0 +1,29 @@
+//
+//  FullImageTableViewCell.swift
+//  Loan
+//
+//  Created by gaowei on 2018/5/3.
+//  Copyright © 2018年 yyl. All rights reserved.
+//
+
+import UIKit
+
+class FullImageViewStyleCell: UITableViewCell, CellStyleProtocol {
+
+    var viewModel: CellStyleModel?
+    func setData(data: Any?, title: String?) {
+        
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
